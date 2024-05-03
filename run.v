0454cd5b4b045d78c7558916207a67bf665b4f04
bstnbuck <59334447+bstnbuck@users.noreply.github.com>
2024-05-03 22:53:13 +0200

    fix deprecated fn

--- run.v
+++ run.v
@@ -19,7 +19,7 @@ fn run(times int, nulls string, mut blockchain []Block) {
 			blockchain << new_block
 			write_to_blockchain_file(filename, output)
 			println('\n' + output + '\n>> Block is valid! <<\n')
-			duration := timestamp_stop.unix_time_milli() - timestamp_start.unix_time_milli()
+			duration := timestamp_stop.unix_milli() - timestamp_start.unix_milli()
 			print('Stopped: ' + timestamp_stop.str())
 			output_duration := '\nTime to make new Block: ' + duration.str() + 'ms => ' +
 				(duration / 1000).str() + 'sec'
